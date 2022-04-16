8c8a9eae3b8b099fcae3beb800025843f755a88c
DanaRyazanova09 <103820817+DanaRyazanova09@users.noreply.github.com>
2022-04-16 12:24:51 +0300

    Create map.js

---
+++ js/map.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+   var clickicon = $('.icon');
+   var showsquare = $('.offsquare');
+   clickicon.mouseenter(function () {
+       showsquare.show();
+   });
+
+    clickicon.mouseleave(function () {
+        showsquare.hide();
+    });
+}); 
+
+$(document).ready(function() {
+   var clickicon = $('.icon2');
+   var showsquare = $('.offsquare2');
+   clickicon.mouseenter(function () {
+       showsquare.show();
+   });
+
+    clickicon.mouseleave(function () {
+        showsquare.hide();
+    });
+});
+
+$(document).ready(function() {
+   var clickicon = $('.icon3');
+   var showsquare = $('.offsquare3');
+   clickicon.mouseenter(function () {
+       showsquare.show();
+   });
+
+    clickicon.mouseleave(function () {
+        showsquare.hide();
+    });
+});
+
+$(document).ready(function() {
+   var clickicon = $('.icon4');
+   var showsquare = $('.offsquare4');
+   clickicon.mouseenter(function () {
+       showsquare.show();
+   });
+
+    clickicon.mouseleave(function () {
+        showsquare.hide();
+    });
+});
+
+$(document).ready(function() {
+   var clickicon = $('.icon5');
+   var showsquare = $('.offsquare5');
+   clickicon.mouseenter(function () {
+       showsquare.show();
+   });
+
+    clickicon.mouseleave(function () {
+        showsquare.hide();
+    });
+});
